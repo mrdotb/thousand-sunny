c64eccdc327fc602a96a1779aac59212e51fc17c
mrdotb <mrdotb@protonmail.com>
2023-11-21 22:19:05 +0100

    ansible server setup

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -12,52 +12,63 @@ init() {
   ansible-galaxy install -r "$ANSIBLE_DIR/requirements.yaml" -f
 }
 
-list() {
-  ansible all -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" --list-hosts
-}
-
-ping() {
-  ansible all -i "${ANSIBLE_INVENTORY_DIR}/hosts.yaml" --one-line -m 'ping'
-}
-
 setup() {
   ansible-playbook -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" "$ANSIBLE_PLAYBOOK_DIR/setup.yaml"
 }
 
+list() {
+  ansible \
+    --ask-become-pass \
+    all -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" --list-hosts
+}
+
+ping() {
+  ansible \
+    --ask-become-pass \
+    all -i "${ANSIBLE_INVENTORY_DIR}/hosts.yaml" --one-line -m 'ping'
+}
+
 kubernetes() {
-  ansible-playbook -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" "$ANSIBLE_PLAYBOOK_DIR/kubernetes.yaml"
+  ansible-playbook \
+    --ask-become-pass \
+    -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" "$ANSIBLE_PLAYBOOK_DIR/kubernetes.yaml"
+}
+
+backup() {
+  ansible-playbook \
+    --ask-become-pass \
+    -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" "$ANSIBLE_PLAYBOOK_DIR/backup.yaml"
 }
 
 status() {
-  ansible-playbook -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" "$ANSIBLE_PLAYBOOK_DIR/status.yaml"
+  ansible-playbook \
+    --ask-become-pass \
+    -i "$ANSIBLE_INVENTORY_DIR/hosts.yaml" "$ANSIBLE_PLAYBOOK_DIR/status.yaml"
 }
 
 case "$1" in
   init)
     init
     ;;
-  list)
-    list
-    ;;
   setup)
     setup
     ;;
-  kubernetes)
-    kubernetes
-    ;;
-  status)
-    status
+  list)
+    list
     ;;
   ping)
     ping
     ;;
-  reboot)
-    reboot
+  kubernetes)
+    kubernetes
     ;;
-  update|upgrade)
-    update_deps
+  backup)
+    backup
+    ;;
+  status)
+    status
     ;;
   *)
-    echo "Usage: $0 {init|list|setup|kubernetes|status|ping|reboot|update|upgrade}"
+    echo "Usage: $0 {init|list|setup|kubernetes|backup|status|ping}"
     exit 1
 esac
